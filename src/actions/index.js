77e7b4b10bc3b7c22b007dd7146c1935a0873513
Amer Hidanović <amerhi@maestralsolutions.com>
2018-02-23 14:07:08 +0100

    before issue with promise

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '0ddf8ec4dec48ec1ffd6806ce6d96ec3';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FEATCH_WEATHER = 'FEATCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},ba`;
+  const request = axios.get(url)
+
+  console.log('request: ', request);
+
+  return {
+    type: FEATCH_WEATHER,
+    paylod: request
+  };
+}
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,5 @@
+export default function WeatherReducer (state = null, action) {
+  
+  console.log('action recived: ',action);
+  return state;
+}
